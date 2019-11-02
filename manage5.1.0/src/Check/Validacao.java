018d15fbe3b57796d3acd73263a6742f7888f412
leandrocnb <leandrobertoldi@alunos.utfpr.edu.br>
2019-11-02 15:53:46 -0300

    ValidaRG

--- manage5.1.0/src/Check/Validacao.java
+++ manage5.1.0/src/Check/Validacao.java
@@ -10,8 +10,26 @@ package Check;
  * @author leandrocnb
  */
 public class Validacao {
-    public boolean validaNome(String nome){
-        
-    }
     
+    public boolean validaRG(String rg){
+        if(rg.contains("^[a-Z]")){
+            return false;
+        }
+        else if(rg.contains("“”!@#$%&*(),=+;")){
+            return false;
+        }
+        else return !(rg.length() < 12 || rg.length() > 12);
+    }    
+
+    
+    
+    public static void main(String args[]) {
+        Validacao valida = new Validacao();
+        if(valida.validaRG("8723kahs893j")){
+            System.out.println("RG Válido");
+        }
+        else{
+            System.out.println("RG Inválido");
+        }
+    }
 }
