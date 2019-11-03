fd8c20388e2852d84a5978d8e8dd96159d804c36
Caioiwamoto <caiokiwamoto@gmail.com>
2019-11-03 04:57:59 -0300

    validaFuncionario

--- manage5.1.0/src/Check/Validacao.java
+++ manage5.1.0/src/Check/Validacao.java
@@ -11,6 +11,20 @@ package Check;
  */
 public class Validacao {
     
+    //Método para valida Nome
+    public boolean validaNome(String nome){
+        if(nome.length() < 5 || nome.length() > 50){
+            return false;
+        }
+        else if(nome.matches(".*[!|@|#|$|%|&|*|(|)|=|+|;].*")){
+            return false;
+        }
+        else if(nome.matches(".*[0|1|2|3|4|5|6|7|8|9].*")){
+            return false;
+        }
+        return true;
+    }
+    
     //Método para valida RG
     public boolean validaRG(String rg){
         String RG = "\\d\\d.\\d\\d\\d.\\d\\d\\d-\\d";
@@ -25,7 +39,36 @@ public class Validacao {
         }
         return true;
     }
-
+    //Método para valida CPF
+    public boolean validaCPF(String cpf){
+        String CPF = "\\d\\d\\d.\\d\\d\\d.\\d\\d\\d-\\d\\d";
+        if(cpf.matches(".*\\D^.^-.*")){
+            return false;
+        }
+        else if(cpf.length() < 14 || cpf.length() > 14){
+            return false;
+        }
+        else if(!cpf.matches(CPF)){
+            return false;
+        }
+        return true;
+    }
+    
+    //Método para valida Data de Nascimento
+    public boolean validaDataDeNascimento(String dataDeNascimento){
+        String Ddn = "\\d\\d/\\d\\d/\\d\\d\\d\\d";
+        if(dataDeNascimento.matches(".*\\D^/.*")){
+            return false;
+        }
+        else if(dataDeNascimento.length() < 10 || dataDeNascimento.length() > 10){
+            return false;
+        }
+        else if(!dataDeNascimento.matches(Ddn)){
+            return false;
+        }
+        return true;
+    }
+    
     //Método para validar Carteira de Trabalho
     public boolean validaCarteiraDeTrabalho(String carteiraDeTrabalho){
         String Cdt = "\\d\\d\\d\\d\\d\\d\\d-\\d\\d\\d\\d\\d";
@@ -36,6 +79,21 @@ public class Validacao {
             return false;
         }
         else if(!carteiraDeTrabalho.matches(Cdt)){
+            return false;
+        }
+        return true;
+    }
+    
+    //Método para validar CEP
+    public boolean validaCEP(String cep){
+        String CEP = "\\d\\d\\d\\d\\d-\\d\\d\\d";
+        if(cep.matches(".*\\D^-.*")){
+            return false;
+        }
+        else if(cep.length() < 9 || cep.length() > 9){
+            return false;
+        }
+        else if(!cep.matches(CEP)){
             return false;
         }
         return true;
@@ -52,6 +110,17 @@ public class Validacao {
         return true;
     }
     
+    //Método para validar Numero
+    public boolean validaNumero(String numero){
+        if(numero.length() < 1 || numero.length() > 6){
+            return false;
+        }
+        else if(numero.matches(".*[!|@|#|$|%|&|*|(|)|=|+|;].*")){
+            return false;
+        }
+        return true;
+    }
+    
     //Método para validar Complemento
     public boolean validaComplemento(String complemento){
         if(complemento.length() > 50){
@@ -63,12 +132,34 @@ public class Validacao {
         return true;
     }
     
+    //Método para validar Bairro
+    public boolean validaBairro(String bairro){
+        if(bairro.length() < 2 || bairro.length() > 50){
+            return false;
+        }
+        else if(bairro.matches(".*[!|@|#|$|%|&|*|(|)|=|+|;].*")){
+            return false;
+        }
+        return true;
+    }
+    
     //Método para validar Cidade
     public boolean validaCidade(String cidade){
         if(cidade.length() < 2 || cidade.length() > 50){
             return false;
         }
         else if(cidade.matches(".*[!|@|#|$|%|&|*|(|)|=|+|;].*")){
+            return false;
+        }
+        return true;
+    }
+    
+    //Método para validar Estado
+    public boolean validaEstado(String estado){
+        if(estado.length() < 2 || estado.length() > 2){
+            return false;
+        }
+        else if(estado.matches(".*[!|@|#|$|%|&|*|(|)|=|+|;].*")){
             return false;
         }
         return true;
@@ -107,7 +198,7 @@ public class Validacao {
     public static void main(String args[]) {
         Validacao valida = new Validacao();
         
-        if(valida.validaLogradouro("")){
+        if(valida.validaEstado("as")){
             System.out.println("Logradouro Válido");
         }
         else{
